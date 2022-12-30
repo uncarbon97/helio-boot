6bca0a31a5398016314838318fb29e8d1224ed3c
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2022-12-30 15:26:40 +0800

    chore: 完善DB初始化及版本更新SQL文件

--- attachments/db/MySQL/helio_boot.sql
+++ attachments/db/MySQL/helio_boot.sql
@@ -467,31 +467,25 @@ CREATE TABLE `oss_file_info`
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '上传文件信息' ROW_FORMAT = Dynamic;
 
-
--- v1.7.2 - Uncarbon - 新增上传文件信息管理菜单
-INSERT INTO `sys_menu`(`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
-                       `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
-                       `external_link`)
-VALUES (20220922152714, NULL, 1, 0, '2022-09-22 15:27:14', 'helio-generator', '2022-09-22 15:27:14',
-        'helio-generator', '上传文件信息管理', 0, 1, 'OssFileInfo', 'ant-design:flag-outlined', 100, 1,
-        '/oss/OssFileInfo/index', '');
-INSERT INTO `sys_menu`(`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
-                       `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
-                       `external_link`)
-VALUES (20220922152715, NULL, 1, 0, '2022-09-22 15:27:14', 'helio-generator', '2022-09-22 15:27:14',
-        'helio-generator', '查询', 1572848462892752896, 2, 'OssFileInfo:retrieve', NULL, 1, 1, NULL, '');
-INSERT INTO `sys_menu`(`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
-                       `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
-                       `external_link`)
-VALUES (20220922152716, NULL, 1, 1, '2022-09-22 15:27:14', 'helio-generator', '2022-09-22 15:35:48', 'admin',
-        '新增', 1572848462892752896, 2, 'OssFileInfo:create', NULL, 2, 1, NULL, '');
-INSERT INTO `sys_menu`(`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
-                       `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
-                       `external_link`)
-VALUES (20220922152717, NULL, 1, 1, '2022-09-22 15:27:14', 'helio-generator', '2022-09-22 15:35:55', 'admin',
-        '编辑', 1572848462892752896, 2, 'OssFileInfo:update', NULL, 4, 1, NULL, '');
-INSERT INTO `sys_menu`(`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
-                       `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
-                       `external_link`)
-VALUES (20220922152718, NULL, 1, 0, '2022-09-22 15:27:14', 'helio-generator', '2022-09-22 15:36:03', 'admin',
-        '删除', 1572848462892752896, 2, 'OssFileInfo:delete', NULL, 2, 1, NULL, '');
+-- v1.7.2 - Uncarbon - 新增上传文件信息后台管理菜单
+INSERT INTO `sys_menu` (`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
+                        `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
+                        `external_link`)
+VALUES (20220922152710, NULL, 1, 0, '2022-12-25 23:09:30', 'admin', '2022-12-26 22:02:31', 'admin', '文件管理', 0, 0,
+        'Oss', 'ant-design:file-outlined', 4, 1, NULL, '');
+INSERT INTO `sys_menu` (`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
+                        `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
+                        `external_link`)
+VALUES (20220922152714, NULL, 1, 0, '2022-09-22 15:27:14', 'helio-generator', '2022-12-26 22:03:14', 'admin',
+        '上传文件信息管理', 20220922152710, 1, 'OssFileInfo', 'ant-design:save-twotone', 1, 1, '/oss/OssFileInfo/index',
+        '');
+INSERT INTO `sys_menu` (`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
+                        `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
+                        `external_link`)
+VALUES (20220922152715, NULL, 1, 0, '2022-09-22 15:27:14', 'helio-generator', '2022-12-26 22:01:58', 'admin', '查询',
+        20220922152714, 2, 'OssFileInfo:retrieve', NULL, 1, 1, NULL, '');
+INSERT INTO `sys_menu` (`id`, `tenant_id`, `revision`, `del_flag`, `created_at`, `created_by`, `updated_at`,
+                        `updated_by`, `title`, `parent_id`, `type`, `permission`, `icon`, `sort`, `status`, `component`,
+                        `external_link`)
+VALUES (20220922152718, NULL, 1, 0, '2022-09-22 15:27:14', 'helio-generator', '2022-12-26 22:02:09', 'admin', '删除',
+        20220922152714, 2, 'OssFileInfo:delete', NULL, 2, 1, NULL, '');
