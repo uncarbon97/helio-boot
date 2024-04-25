020c3eee1106c25a376271408253465db2fc6f88
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2024-04-25 13:49:50 +0800

    fix: https://github.com/uncarbon97/helio-admin-vue-vben/issues/22

--- src/main/java/cc/uncarbon/module/sys/service/SysMenuService.java
+++ src/main/java/cc/uncarbon/module/sys/service/SysMenuService.java
@@ -43,6 +43,9 @@ public class SysMenuService {
     private final SysMenuMapper sysMenuMapper;
     private final SysRoleMenuRelationService sysRoleMenuRelationService;
 
+    /**
+     * 仅用于输出一个，可按时间流逝而增长的纯数字，避免重复
+     */
     private static final Snowflake SNOWFLAKE = IdUtil.getSnowflake(0L, 0L);
 
 
@@ -247,7 +250,7 @@ public class SysMenuService {
 
         String snowflakeIdStr = SNOWFLAKE.nextIdStr();
         bo
-                .setName(snowflakeIdStr)
+                .setName(bo.getTitle())
                 .setMeta(new VbenAdminMenuMetaVO(bo.getTitle(), false, bo.getIcon()));
 
         switch (bo.getType()) {
