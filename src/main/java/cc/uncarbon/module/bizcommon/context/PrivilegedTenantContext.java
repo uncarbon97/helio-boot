cc31cab4efd51301c2b65cd1f430127dbfd18f9d
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2025-06-08 10:53:20 +0800

    bump: 发布2.4.0
    details see: https://helio.uncarbon.cc/#/i18n/zh-CN/appendix/change-log

--- src/main/java/cc/uncarbon/module/bizcommon/context/PrivilegedTenantContext.java
+++ src/main/java/cc/uncarbon/module/bizcommon/context/PrivilegedTenantContext.java
@@ -4,17 +4,18 @@ import cc.uncarbon.framework.core.constant.HelioConstant;
 import cc.uncarbon.framework.core.context.TenantContext;
 
 /**
- * 快速构造一个超级租户实例，用于临时绕过租户拦截器；同时提供一个单例对象
+ * 快速构造一个超级租户上下文，用于临时绕过租户拦截器；同时提供一个单例对象
  * 使用示例
- * <p>
  *     TenantContext originContext = TenantContextHolder.getTenantContext();
  *     try {
- *         TenantContextHolder.setTenantContext(PrivilegedTenantContext.SINGLETON);
+ *         if (originContext == null) {
+ *             // 用户可能尚未登录，但需要查库（如获取APP首页轮播图等），得绕过租户限制；但如果已经登录了，则不必切换租户态
+ *             TenantContextHolder.setTenantContext(PrivilegedTenantContext.SINGLETON);
+ *         }
  *         // 业务代码...
  *     } finally {
  *         TenantContextHolder.setTenantContext(originContext);
  *     }
- * </p>
  */
 public class PrivilegedTenantContext extends TenantContext {
 
